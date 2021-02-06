bc994832dc97ce6e818058ce54784c7e9a0752d8
2san <chillout2san@gmai.com>
2021-02-06 17:36:16 +0900

    js4質問②

--- main.js
+++ main.js
@@ -1,108 +1,113 @@
 'use strict'
 
 // ホーム画面のDOM
-const $homeContainer = document.querySelector('#home-container');
-const $main = document.querySelector('#main');
-const $para = document.querySelector('#para');
-const $btn = document.querySelector('#btn');
+const homeContainer = document.getElementById('home-container');
+const main = document.getElementById('main');
+const para = document.getElementById('para');
+const btn = document.getElementById('btn');
 
 // 問題画面のDOM
-const $questionContainer = document.querySelector('#question-container');
-const $questionAnswerContainer = document.querySelector('#question-answers-container');
-const $quizAnswerBtn = document.querySelectorAll('#question-answers-container > button');
-const $quizTitle = document.querySelector('#quiz-title');
-const $quizGenre = document.querySelector('#quiz-genre');
-const $quizDif = document.querySelector('#quiz-dif');
-const $quizQuestion = document.querySelector('#question');
-const $quizAnswer0 = document.querySelector('#answer0');
-const $quizAnswer1 = document.querySelector('#answer1');
-const $quizAnswer2 = document.querySelector('#answer2');
-const $quizAnswer3 = document.querySelector('#answer3');
+const questionContainer = document.getElementById('question-container');
+const questionAnswerContainer = document.getElementById('question-answers-container');
+const quizAnswerBtn = document.querySelectorAll('#question-answers-container > button');
+const quizTitle = document.getElementById('quiz-title');
+const quizGenre = document.getElementById('quiz-genre');
+const quizDif = document.getElementById('quiz-dif');
+const quizQuestion = document.getElementById('question');
+const quizAnswer0 = document.getElementById('answer0');
+const quizAnswer1 = document.getElementById('answer1');
+const quizAnswer2 = document.getElementById('answer2');
+const quizAnswer3 = document.getElementById('answer3');
 
 // 結果画面のDOM
-const $result = document.querySelector('#result');
-const $resultTitle = document.querySelector('#result-title');
-const $resultPara = document.querySelector('#result-para');
-const $resultBtn = document.querySelector('#result-btn');
-
-// 正解数用のカウンター
-let correctCounter = 0;
-
-// 何問目か用のカウンター
-let quizCounter = 0;
-
-// クイズ要素
-let quizObj = [];
+const result = document.getElementById('result');
+const resultTitle = document.getElementById('result-title');
+const resultPara = document.getElementById('result-para');
+const resultBtn = document.getElementById('result-btn');
 
 // 開始ボタンを押下すると実行する非同期処理
-$btn.addEventListener('click', () => {
-  $main.innerText = '取得中';
-  $para.innerText = '少々お待ちください';
-  $btn.classList.add('transparent');
+btn.addEventListener('click', () => {
+  main.innerText = '取得中';
+  para.innerText = '少々お待ちください';
+  btn.classList.add('transparent');
   getQuizData();
+  setQuiz();
+  // getuQuizDataで取得したデータをどうやってこちらに持ってくる？？
 });
 
 // クイズを出題するクラス
 class Quiz {
-  constructor(obj, i) {
-    this.index = i + 1;
-    this.quizGenre = obj.category;
-    this.quizDif = obj.difficulty;
-    this.question = obj.question;
-    obj.incorrect_answers.push(obj.correct_answer);
-    this.answers = obj.incorrect_answers;
+  constructor(obj) {
+    this.quizObj = obj;
+    this.correctCounter = 0;
   };
-};
-
-// 選択肢をシャッフルする関数
-const shuffleAns = (answers) => {
-  for(let i = answers.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const tmp = answers[i];
-    answers[i] = answers[j];
-    answers[j] = tmp;
+  displayQuizNumber(i){
+    return i;
+  }
+  displayQuestion(i){
+    const question = this.quizObj[i].question;
+    return question;
   };
+  displayDifficulty(i){
+    const difficulty = this.quizObj[i].difficulty;
+    return difficulty;
+  };
+  displayChoices(i){
+    const choices = this.quizObj[i].incorrect_answers.concat('correct_answer');
+    for(let i = choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const tmp = choices[i];
+      choices[i] = choices[j];
+      choices[j] = tmp;
+    };
+    return choices;
+  };
+  displayCorrectAnswer(i){
+    const correctAnswer = this.quizObj[i].correct_answer;
+    return correctAnswer;
+  }
+  displayCorrectCounter(){
+    return correctCounter;
+  }
 };
 
 // クイズを表示する関数
 const setQuiz = (obj) => {
-  $homeContainer.classList.add('transparent');
-  $questionContainer.classList.remove('transparent');
-  $questionAnswerContainer.classList.remove('transparent');
-  $quizTitle.innerHTML = `問題${obj.index}`;
-  $quizGenre.innerHTML = `[ジャンル]${obj.quizGenre}`;
-  $quizDif.innerHTML = `[難易度]${obj.quizDif}`;
-  $quizQuestion.innerHTML = obj.question;
-  $quizAnswer0.innerHTML = obj.answers[0];
-  $quizAnswer1.innerHTML = obj.answers[1];
-  $quizAnswer2.innerHTML = obj.answers[2];
-  $quizAnswer3.innerHTML = obj.answers[3];
+  homeContainer.classList.add('transparent');
+  questionContainer.classList.remove('transparent');
+  questionAnswerContainer.classList.remove('transparent');
+  quizTitle.innerHTML = `問題${obj.index}`;
+  quizGenre.innerHTML = `[ジャンル]${obj.quizGenre}`;
+  quizDif.innerHTML = `[難易度]${obj.quizDif}`;
+  quizQuestion.innerHTML = obj.question;
+  quizAnswer0.innerHTML = obj.answers[0];
+  quizAnswer1.innerHTML = obj.answers[1];
+  quizAnswer2.innerHTML = obj.answers[2];
+  quizAnswer3.innerHTML = obj.answers[3];
   if(obj.answers.length === 2) {
-    $quizAnswer2.classList.add('transparent');
-    $quizAnswer3.classList.add('transparent');
+    quizAnswer2.classList.add('transparent');
+    quizAnswer3.classList.add('transparent');
   }else{
-    $quizAnswer2.classList.remove('transparent');
-    $quizAnswer3.classList.remove('transparent');
+    quizAnswer2.classList.remove('transparent');
+    quizAnswer3.classList.remove('transparent');
   };
 };
 
-// APIを取得する
+// APIからデータを取得する
 const getQuizData = async () => {
   try{
     const response = await fetch('https://opentdb.com/api.php?amount=10');
     const quizMass = await response.json();
-    quizObj = quizMass.results;
-    const quiz = new Quiz(quizObj[0], 0);
-    shuffleAns(quiz);
-    setQuiz(quiz);
-  }catch(e) {
-      console.log('e');
+    const quiz = new Quiz(quizMass.results);
+    return quiz;
+  }catch(error) {
+      console.log('error');
     };
 };
 
 // 選択肢ボタンを押すと次のクイズが出題される
 for(let i = 0; i < 4; i++){
-    $quizAnswerBtn[i].addEventListener('click', (e) => {
+    quizAnswerBtn[i].addEventListener('click', (e) => {
       if(quizCounter === 0){
         if(e.target.innerHTML === quizObj[0].correct_answer){
           correctCounter++;
@@ -119,23 +124,24 @@ for(let i = 0; i < 4; i++){
       if(quizCounter + 1 < quizObj.length){
         quizCounter++;
         const quiz = new Quiz(quizObj[quizCounter],quizCounter);
+        quiz.shuffleAns();
         setQuiz(quiz);
       }else{
-        $questionContainer.classList.add('transparent');
-        $questionAnswerContainer.classList.add('transparent');
-        $result.classList.remove('transparent');
-        $resultTitle.innerHTML = `あなたの正答数は${correctCounter}です！`;
+        questionContainer.classList.add('transparent');
+        questionAnswerContainer.classList.add('transparent');
+        result.classList.remove('transparent');
+        resultTitle.innerHTML = `あなたの正答数は${correctCounter}です！`;
       };
     });
 };
 
 // ホームボタンを押すとはじめに戻る
-$resultBtn.addEventListener('click', () => {
+resultBtn.addEventListener('click', () => {
   result.classList.add('transparent');
-  $homeContainer.classList.remove('transparent');
-  $btn.classList.remove('transparent');
-  $main.innerText = 'ようこそ';
-  $para.innerText = '以下のボタンをクリック';
+  homeContainer.classList.remove('transparent');
+  btn.classList.remove('transparent');
+  main.innerText = 'ようこそ';
+  para.innerText = '以下のボタンをクリック';
   quizCounter = 0;
   correctCounter = 0;
-})
+});
